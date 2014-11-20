9fe96b0fb9800a59f9180c5ff99c1b7ba95c7340
Cathy Deng <cathyd125@gmail.com>
2014-11-20 16:30:12 -0600

    cleaning up utils

--- parserator/utils.py
+++ parserator/utils.py
@@ -4,21 +4,21 @@ from sklearn.base import BaseEstimator
 from sklearn.grid_search import GridSearchCV
 from training import get_data_sklearn_format
 import pycrfsuite
-import usaddress
+import config
 
 
 def f1_with_flattening(estimator, X, y):
     """
     Calculate F1 score by flattening the predictions of the
-    estimator across all addresses. For example, given the following
-    addresses as input
+    estimator across all sequences. For example, given the following
+    address sequences as input
         ['1 My str', '2 Your blvd'],
     the predictions of the model will be flattened like so:
         ['AddressNumber', 'StreetName', 'StreetNamePostType', 'AddressNumber', 'StreetName', 'StreetNamePostType']
     and compared to a similarly flattened gold standard labels. This calculates the overall
-    quality of the model across all addresses as opposed to how well it does
-    at any particular address.
-    :param X: list of addresses to tag
+    quality of the model across all sequences as opposed to how well it does
+    at any particular sequence.
+    :param X: list of sequences to tag
     :param y: list of gold standard tuples
     """
     predicted = estimator.predict(X)
@@ -30,11 +30,11 @@ def f1_with_flattening(estimator, X, y):
     return f1_score(flat_gold, flat_pred)
 
 
-class AddressEstimator(BaseEstimator):
+class SequenceEstimator(BaseEstimator):
     """
-    A sklearn-compatible wrapper for a usaddress trainer
+    A sklearn-compatible wrapper for a parser trainer
     """
-    model_path = 'usaddress/usaddr.crfsuite'
+    model_path = os.path.split(os.path.abspath(__file__))[0] + '/' + config.MODEL_FILE
 
     def __init__(self, c1=1, c2=1, feature_minfreq=0):
         """
@@ -52,24 +52,24 @@ class AddressEstimator(BaseEstimator):
         # an we can't have a full stop there. Replace with an underscore
         params = {k.replace('_', '.'): v for k, v in self.__dict__.items()}
         trainer = pycrfsuite.Trainer(verbose=False, params=params)
-        for address, labels in zip(X, y):
-            tokens = usaddress.tokenize(address)
-            trainer.append(usaddress.addr2features(tokens), labels)
+        for raw_text, labels in zip(X, y):
+            tokens = tokenize(raw_text)
+            trainer.append(tokens2features(tokens), labels)
         trainer.train(self.model_path)
-        reload(usaddress)
+        reload(parserator)
 
     def predict(self, X):
-        reload(usaddress)  # tagger object is defined at the module level, update now
+        reload(parserator)  # tagger object is defined at the module level, update now
         predictions = []
-        for address in X:
-            predictions.append([foo[1] for foo in usaddress.parse(address)])
+        for sequence in X:
+            predictions.append([foo[1] for foo in parserator.parse(sequence)])
         return predictions
 
 
 if __name__ == '__main__':
     # refer to http://www.chokkan.org/software/crfsuite/manual.html
     # for description of parameters
-    cv = GridSearchCV(AddressEstimator(), {'c1': [10 ** x for x in range(-2, 2)],
+    cv = GridSearchCV(SequenceEstimator(), {'c1': [10 ** x for x in range(-2, 2)],
                                            'c2': [10 ** x for x in range(-2, 4)],
                                            'feature_minfreq': [0, 3, 5]},
                       scoring=f1_with_flattening, verbose=5)
