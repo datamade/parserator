5f0b1397803e8202044cbf9bbe3bfd60a0b7258d
Cathy Deng <cathyd125@gmail.com>
2014-11-20 16:29:42 -0600

    cleaning up training

--- parserator/training.py
+++ parserator/training.py
@@ -1,11 +1,10 @@
 import pycrfsuite
-import usaddress
 import random
 import os
 from lxml import etree
 from imp import reload
+import config
 
-NULL_TAG = 'Null'
 
 def trainModel(training_data, model_file,
                params_to_set={'c1':0.1, 'c2':0.01, 'feature.minfreq':0}):
@@ -13,9 +12,9 @@ def trainModel(training_data, model_file,
     X = []
     Y = []
 
-    for address_text, components in training_data:
+    for raw_string, components in training_data:
         tokens, labels = zip(*components)
-        X.append(usaddress.addr2features(tokens))
+        X.append(tokens2features(tokens))
         Y.append(labels)
 
     # train model
@@ -24,22 +23,22 @@ def trainModel(training_data, model_file,
         trainer.append(xseq, yseq)
 
     trainer.train(model_file)
-    reload(usaddress)
+    reload(parserator)
 
 def parseTrainingData(filepath):
     tree = etree.parse(filepath)
-    address_collection = tree.getroot()
+    collection = tree.getroot()
 
-    for address in address_collection:
-        address_components = []
-        address_text = etree.tostring(address, method='text')
-        address_text = address_text.replace('&#38;', '&')
-        for component in list(address):
-            address_components.append([component.text, component.tag])
+    for sequence in collection:
+        sequence_components = []
+        raw_text = etree.tostring(sequence, method='text')
+        raw_text = raw_text.replace('&#38;', '&')
+        for component in list(sequence):
+            sequence_components.append([component.text, component.tag])
             if component.tail and component.tail.strip():
-                address_components.append([component.tail.strip(), NULL_TAG])
+                sequence_components.append([component.tail.strip(), config.NULL_TAG])
 
-        yield address_text, address_components
+        yield raw_text, sequence_components
 
 
 def get_data_sklearn_format(path='training/training_data/labeled.xml'):
@@ -47,18 +46,18 @@ def get_data_sklearn_format(path='training/training_data/labeled.xml'):
     Parses the specified data file and returns it in sklearn format.
     :param path:
     :return: tuple of:
-                1) list of training addresses, each of which is a string
+                1) list of training sequences, each of which is a string
                 2) list of gold standard labels, each of which is a tuple
                 of strings, one for each token in the corresponding training
-                address
+                sequence
     """
     data = list(parseTrainingData(path))
     random.shuffle(data)
 
     x, y = [], []
-    for address_text, components in data:
+    for raw_string, components in data:
         tokens, labels = zip(*components)
-        x.append(address_text)
+        x.append(raw_string)
         y.append(labels)
     return x, y
 
@@ -66,3 +65,14 @@ def train(example_file, settings_file_path) :
 
     training_data = list(parseTrainingData(example_file))
     trainModel(training_data, settings_file_path)
+
+
+
+if __name__ == '__main__':
+
+    root_path = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+
+    training_data = list(parseTrainingData(root_path + '/training/training_data/' + config.TRAIN_DATA))
+    # ********* make training & training data directory?
+
+    trainModel(training_data, root_path + '/parserator/' + config.MODEL_FILE)
