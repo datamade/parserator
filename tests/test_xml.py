f24c3df9017fb2440534ded73d2b20b8765a11c1
Cathy Deng <cathyd125@gmail.com>
2014-12-04 12:02:10 -0600

    add tests

---
+++ tests/test_xml.py
@@ -0,0 +1,24 @@
+from parserator.data_prep_utils import sequence2XML
+from lxml import etree
+import unittest
+
+class TestList2XML(unittest.TestCase) :
+
+    def test_xml(self):
+        XMLequals( [('#', 'foo'), ('1', 'foo'), ('Pinto', 'foo')], '<foo>#</foo> <foo>1</foo> <foo>Pinto</foo>')
+
+    def test_none_tag(self):
+        XMLequals( [('Box', 'foo'), ('#', 'Null'), ('1', 'foo'), ('Pinto', 'foo')], '<foo>Box</foo> <Null>#</Null> <foo>1</foo> <foo>Pinto</foo>')
+        XMLequals( [('#', 'Null'), ('1', 'foo'), ('Pinto', 'foo')], '<Null>#</Null> <foo>1</foo> <foo>Pinto</foo>')
+       
+       
+def XMLequals(labeled_sequence, xml):
+    correct_xml = '<TokenSequence>' + xml + '</TokenSequence>'
+    generated_xml = etree.tostring( sequence2XML(labeled_sequence, 'TokenSequence') )
+    print "Correct:   ", correct_xml
+    print "Generated: ", generated_xml
+    assert correct_xml == generated_xml
+
+
+if __name__ == '__main__' :
+    unittest.main()    
