60d427cf712511e1af9c9d63d5a954c015f8ef4e
Brent Payne <brent.payne@gmail.com>
2015-01-16 13:30:49 -0800

    update versoin to 0.3 made setup.py utilize requirements.txt moved nose to test_requirements.txt

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@ try:
     from setuptools import setup
 except ImportError :
     raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements(<requirements_path>)
+
+# reqs is a list of requirement
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    version='0.2',
+    version='0.3',
     url='https://github.com/datamade/parserator',
     description='Create parsers',
     name='parserator',
     packages=['parserator'],
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
-    install_requires=['python-crfsuite>=0.7',
-                      'lxml'],
+    install_requires=reqs,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
