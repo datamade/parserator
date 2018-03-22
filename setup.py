93893261c5d8824eb77e4e88a103efe475d54566
Forest Gregg <fgregg@uchicago.edu>
2018-03-22 15:45:01 -0500

    v0.6.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.version < '3':
 
 
 setup(
-    version='0.6.5',
+    version='0.6.6',
     url='https://github.com/datamade/parserator',
     description='Create parsers',
     name='parserator',
