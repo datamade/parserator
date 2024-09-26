4c5728de42037eec7db456a2b67d25cfd3af2d85
Forest Gregg <fgregg@users.noreply.github.com>
2024-09-26 12:05:58 -0400

    tidying up (#53)

--- parserator/main.py
+++ parserator/main.py
@@ -1,26 +1,14 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from __future__ import print_function
-from __future__ import absolute_import
-
-from builtins import open
-
 import argparse
-import os
-import shutil
-import fileinput
-import sys
 import glob
+import os
+import sys
 import textwrap
 
-from lxml import etree
 import chardet
+from lxml import etree
 
-from . import manual_labeling
-from . import training
-from . import parser_template
-from . import data_prep_utils
+from . import data_prep_utils, manual_labeling, parser_template, training
+
 
 def dispatch():
 
@@ -28,37 +16,36 @@ def dispatch():
     parser_subparsers = parser.add_subparsers()
 
     # Arguments for label command
-    sub_label = parser_subparsers.add_parser('label')
-    sub_label.add_argument(dest='infile',
-                           help='input csv filepath for the label task',
-                           type=file_type)
-    sub_label.add_argument(dest='outfile',
-                           help='output xml filepath for the label task',
-                           action=XML)
-    sub_label.add_argument(dest='module',
-                           help='parser module name',
-                           type=python_module)
+    sub_label = parser_subparsers.add_parser("label")
+    sub_label.add_argument(
+        dest="infile", help="input csv filepath for the label task", type=file_type
+    )
+    sub_label.add_argument(
+        dest="outfile", help="output xml filepath for the label task", action=XML
+    )
+    sub_label.add_argument(dest="module", help="parser module name", type=python_module)
     sub_label.set_defaults(func=label)
 
     # Arguments for train command
-    sub_train = parser_subparsers.add_parser('train')
-    sub_train.add_argument(dest='traindata',
-                           help='comma separated xml filepaths, or "path/to/traindata/*.xml"',
-                           type=training_data)
-    sub_train.add_argument(dest='module',
-                           help='parser module name',
-                           type=python_module)
-    sub_train.add_argument('--modelfile',
-                           dest='model_path',
-                           help='location of model file',
-                           action=ModelFile,
-                           required=False)
+    sub_train = parser_subparsers.add_parser("train")
+    sub_train.add_argument(
+        dest="traindata",
+        help='comma separated xml filepaths, or "path/to/traindata/*.xml"',
+        type=training_data,
+    )
+    sub_train.add_argument(dest="module", help="parser module name", type=python_module)
+    sub_train.add_argument(
+        "--modelfile",
+        dest="model_path",
+        help="location of model file",
+        action=ModelFile,
+        required=False,
+    )
     sub_train.set_defaults(func=train)
 
     # Arguments for init command
-    sub_init = parser_subparsers.add_parser('init')
-    sub_init.add_argument(dest='modulename',
-                          help='module name for a new parser')
+    sub_init = parser_subparsers.add_parser("init")
+    sub_init.add_argument(dest="modulename", help="module name for a new parser")
     sub_init.set_defaults(func=init)
 
     if len(sys.argv[1:]) == 0:
@@ -70,18 +57,18 @@ def dispatch():
     args.func(args)
 
 
-    
-def label(args) :
+def label(args):
     manual_labeling.label(args.module, args.infile, args.outfile, args.xml)
 
-def train(args) :
+
+def train(args):
     training_data = args.traindata
     module = args.module
     model_path = args.model_path
 
     if model_path is None:
-        model_path = module.__name__ + '/' +module.MODEL_FILE
-        if hasattr(module, 'MODEL_FILES'):
+        model_path = module.__name__ + "/" + module.MODEL_FILE
+        if hasattr(module, "MODEL_FILES"):
             msg = """
                   NOTE: this parser allows for multiple model files
                   You can specify a model with the --modelfile argument
@@ -94,71 +81,73 @@ def train(args) :
     training.train(module, training_data, model_path)
 
 
-def init(args) :
+def init(args):
     name = args.modulename
     data = "raw"
     training = "training"
-    tests = 'tests'
-    
+    tests = "tests"
+
     dirs_to_mk = [name, data, training, tests]
 
-    print('\nInitializing directories for %s' %name, sys.stderr)
+    print("\nInitializing directories for %s" % name, sys.stderr)
     for directory in dirs_to_mk:
         if not os.path.exists(directory):
             os.mkdir(directory)
-            print('* %s' %directory, sys.stderr)
+            print("* %s" % directory, sys.stderr)
 
-    print('\nGenerating __init__.py', sys.stderr)
-    init_path = name + '/__init__.py'
+    print("\nGenerating __init__.py", sys.stderr)
+    init_path = name + "/__init__.py"
 
     if os.path.exists(init_path):
-        print('  warning: %s already exists' %init_path, sys.stderr)
+        print("  warning: %s already exists" % init_path, sys.stderr)
     else:
         with open(init_path, "w") as f:
             f.write(parser_template.init_template())
-        print('* %s' %init_path)
+        print("* %s" % init_path)
 
-    print('\nGenerating setup.py')
-    if os.path.exists('setup.py'):
-        print('  warning: setup.py already exists', sys.stderr)
+    print("\nGenerating setup.py")
+    if os.path.exists("setup.py"):
+        print("  warning: setup.py already exists", sys.stderr)
     else:
-        with open('setup.py', 'w') as f:
+        with open("setup.py", "w") as f:
             f.write(parser_template.setup_template(name))
-        print('* setup.py', sys.stderr)
+        print("* setup.py", sys.stderr)
 
-    print('\nGenerating test file', sys.stderr)
-    token_test_path = tests+'/test_tokenizing.py'
+    print("\nGenerating test file", sys.stderr)
+    token_test_path = tests + "/test_tokenizing.py"
     if os.path.exists(token_test_path):
-        print('  warning: %s already exists' % token_test_path, sys.stderr)
+        print("  warning: %s already exists" % token_test_path, sys.stderr)
     else:
-        with open(token_test_path, 'w') as f:
+        with open(token_test_path, "w") as f:
             f.write(parser_template.test_tokenize_template(name))
-        print('* %s' %token_test_path, sys.stderr)
+        print("* %s" % token_test_path, sys.stderr)
+
 
 class XML(argparse.Action):
     def __call__(self, parser, namespace, string, option_string):
         try:
-            with open(string, 'r') as f:
+            with open(string) as f:
                 tree = etree.parse(f)
                 xml = tree.getroot()
-        except (OSError, IOError):
+        except OSError:
             xml = None
         except etree.XMLSyntaxError as e:
-            if 'Document is empty' not in str(e):
-                raise argparse.ArgumentError(self,
-                                             "%s does not seem to be a valid xml file"
-                                             % string)
+            if "Document is empty" not in str(e):
+                raise argparse.ArgumentError(
+                    self, "%s does not seem to be a valid xml file" % string
+                )
             xml = None
 
         setattr(namespace, self.dest, string)
-        setattr(namespace, 'xml', xml)
+        setattr(namespace, "xml", xml)
+
 
 def file_type(arg):
     try:
-        f = open(arg, 'rb')
+        f = open(arg, "rb")
     except OSError as e:
-        message = _("can't open '%s': %s")
-        raise ArgumentTypeError(message % (arg, e))
+        message = "can't open '%s': %s"
+        raise argparse.ArgumentTypeError(message % (arg, e))
     else:
         detector = chardet.universaldetector.UniversalDetector()
 
@@ -170,63 +159,74 @@ def file_type(arg):
         f.close()
         detector.close()
 
-        f = open(arg, 'r', encoding=detector.result['encoding'])
+        f = open(arg, encoding=detector.result["encoding"])
 
     return f
 
+
 def training_data(arg):
     all_files = []
-    for path in arg.split(','):
+    for path in arg.split(","):
         all_files.extend(glob.glob(path))
 
-    xml_files = [f for f in all_files
-                 if (f.lower().endswith('.xml')
-                     and os.path.isfile(f))]
+    xml_files = [
+        f for f in all_files if (f.lower().endswith(".xml") and os.path.isfile(f))
+    ]
 
     if not xml_files:
-        raise argparse.ArgumentTypeError('Please specify one or more xml training files (comma separated) [--trainfile FILE]')
+        raise argparse.ArgumentTypeError(
+            "Please specify one or more xml training files (comma separated) [--trainfile FILE]"
+        )
 
     training_data = set()
     for xml_file in xml_files:
-        with open(xml_file, 'r') as f:
+        with open(xml_file) as f:
             try:
                 tree = etree.parse(f)
             except etree.XMLSyntaxError:
-                raise argparse.ArgumentTypeError('%s is not a valid xml file' % (f.name,))
+                raise argparse.ArgumentTypeError(
+                    "{} is not a valid xml file".format(f.name)
+                )
 
             file_xml = tree.getroot()
             training_data.update(data_prep_utils.TrainingData(file_xml))
 
     if not training_data:
-        raise argparse.ArgumentTypeError("No training data found. Perhaps double check "
-                                         "your training data filepaths?")
+        raise argparse.ArgumentTypeError(
+            "No training data found. Perhaps double check "
+            "your training data filepaths?"
+        )
 
     msg = """
           training model on {num} training examples from {file_list} file(s)"""
 
-    print(textwrap.dedent(msg.format(num=len(training_data),
-                                     file_list=xml_files)))
+    print(textwrap.dedent(msg.format(num=len(training_data), file_list=xml_files)))
 
     return training_data
+
 
 class ModelFile(argparse.Action):
     def __call__(self, parser, namespace, model_file, option_string):
         module = namespace.module
 
-        if hasattr(module, 'MODEL_FILES'):
+        if hasattr(module, "MODEL_FILES"):
             try:
-                model_path = module.__name__ + '/'  +module.MODEL_FILES[model_file]
+                model_path = module.__name__ + "/" + module.MODEL_FILES[model_file]
             except KeyError:
                 msg = """
                       Invalid --modelfile argument
                       Models available: %s"""
-                raise argparse.ArgumentTypeError(text.dedent(msg) % module.MODEL_FILES)
+                raise argparse.ArgumentTypeError(
+                    textwrap.dedent(msg) % module.MODEL_FILES
+                )
         else:
-            raise argparse.ArgumentError(self, 'This parser does not allow for multiple models')
+            raise argparse.ArgumentError(
+                self, "This parser does not allow for multiple models"
+            )
 
         setattr(namespace, self.dest, model_path)
+
 
 def python_module(arg):
     module = __import__(arg)
     return module
-
--- parserator/parser_template.py
+++ parserator/parser_template.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 def init_template():
 
     return """\
@@ -16,9 +13,9 @@ from collections import OrderedDict
 
 #  _____________________
 # |1. CONFIGURE LABELS! |
-# |_____________________| 
-#     (\__/) || 
-#     (•ㅅ•) || 
+# |_____________________|
+#     (\\__/) ||
+#     (•ㅅ•) ||
 #     / 　 づ
 LABELS = [] # The labels should be a list of strings
 
@@ -65,9 +62,9 @@ def tag(raw_string) :
 
 #  _____________________
 # |2. CONFIGURE TOKENS! |
-# |_____________________| 
-#     (\__/) || 
-#     (•ㅅ•) || 
+# |_____________________|
+#     (\\__/) ||
+#     (•ㅅ•) ||
 #     / 　 づ
 def tokenize(raw_string):
     # this determines how any given string is split into its tokens
@@ -78,7 +75,7 @@ def tokenize(raw_string):
             raw_string = str(raw_string, encoding='utf-8')
         except:
             raw_string = str(raw_string)
-    
+
     re_tokens = # re.compile( [REGEX HERE], re.VERBOSE | re.UNICODE)
     tokens = re_tokens.findall(raw_string)
 
@@ -89,14 +86,14 @@ def tokenize(raw_string):
 
 #  _______________________
 # |3. CONFIGURE FEATURES! |
-# |_______________________| 
-#     (\__/) || 
-#     (•ㅅ•) || 
+# |_______________________|
+#     (\\__/) ||
+#     (•ㅅ•) ||
 #     / 　 づ
 def tokens2features(tokens):
     # this should call tokenFeatures to get features for individual tokens,
     # as well as define any features that are dependent upon tokens before/after
-    
+
     feature_sequence = [tokenFeatures(tokens[0])]
     previous_features = feature_sequence[-1].copy()
 
@@ -107,11 +104,11 @@ def tokens2features(tokens):
 
         # features for the features of adjacent tokens
         feature_sequence[-1]['next'] = current_features
-        token_features['previous'] = previous_features        
-        
+        token_features['previous'] = previous_features
+
         # DEFINE ANY OTHER FEATURES THAT ARE DEPENDENT UPON TOKENS BEFORE/AFTER
         # for example, a feature for whether a certain character has appeared previously in the token sequence
-        
+
         feature_sequence.append(token_features)
         previous_features = current_features
 
@@ -122,7 +119,7 @@ def tokens2features(tokens):
         feature_sequence[1]['previous']['rawstring.start'] = True
         feature_sequence[-2]['next']['rawstring.end'] = True
 
-    else : 
+    else :
         # a singleton feature, for if there is only one token in a string
         feature_sequence[0]['singleton'] = True
 
@@ -143,7 +140,7 @@ def casing(token) :
     if token.isupper() :
         return 'upper'
     elif token.islower() :
-        return 'lower' 
+        return 'lower'
     elif token.istitle() :
         return 'title'
     elif token.isalpha() :
@@ -151,6 +148,7 @@ def casing(token) :
     else :
         return False
 """
+
 
 def setup_template(module_name):
 
@@ -164,8 +162,8 @@ setup(
     version='0.1',
     url='',
     description='',
-    name='%s',
-    packages=['%s'],
+    name='{}',
+    packages=['{}'],
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
     install_requires=['python-crfsuite>=0.7',
                       'lxml'],
@@ -184,11 +182,16 @@ setup(
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Information Analysis']
 )
-"""%(module_name, module_name)
+""".format(
+        module_name,
+        module_name,
+    )
+
 
 def test_tokenize_template(module_name):
 
-    return """\
+    return (
+        """\
 from %s import tokenize
 import unittest
 
@@ -197,7 +200,7 @@ class TestTokenizing(unittest.TestCase) :
     def test_split_on_punc(self) :
 
         assert tokenize('foo,bar') == ['foo,', 'bar']
-    
+
     def test_spaces(self) :
 
         assert tokenize('foo bar') == ['foo', 'bar']
@@ -206,5 +209,7 @@ class TestTokenizing(unittest.TestCase) :
         assert tokenize(' foo bar') == ['foo', 'bar']
 
 if __name__ == '__main__' :
-    unittest.main()    
-""" %module_name
+    unittest.main()
+"""
+        % module_name
+    )
